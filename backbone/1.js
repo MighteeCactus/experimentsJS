f192ffe846679eee26c6297ce0f10a8a46dc43d2
MighteeCactus <mightee.cactus@gmail.com>
2013-02-26 17:54:45 +0700

    backbone step 5 item actions

--- backbone/1.js
+++ backbone/1.js
@@ -1,5 +1,9 @@
 define(["backbone", "underscore", "jquery"], function(Backbone, _, $) {
 
+    Backbone.sync = function(method, model, success, error) {
+        success();
+    };
+
     var Item = Backbone.Model.extend({
         defaults: {
             part1: "hello",
@@ -15,15 +19,43 @@ define(["backbone", "underscore", "jquery"], function(Backbone, _, $) {
         tagName: "li",
 
         initialize: function() {
-            _.bindAll(this, "render");
+            _.bindAll(this, "render", "unrender", "swap", "remove");
+
+            this.model.bind("change", this.render);
+            this.model.bind("remove", this.unrender);
+        },
+
+        events: {
+            "click span.swap": "swap",
+            "click span.delete": "remove"
         },
 
         render: function() {
-
-            $(this.el).html("<span>" + this.model.get("part1") + " " + this.model.get("part2") + "</span>");
+            $(this.el).html('<span style="color:black;">'+this.model.get('part1')+' '+this.model.get('part2')+'</span> &nbsp; &nbsp; <span class="swap" style="font-family:sans-serif; color:blue; cursor:pointer;">[swap]</span> <span class="delete" style="cursor:pointer; color:red; font-family:sans-serif;">[delete]</span>');
 
             return this;
+        },
+
+        unrender: function() {
+            $(this.el).remove();
+
+            return this;
+        },
+
+        swap: function() {
+            var swapped = {
+                part1: this.model.get("part2"),
+                part2: this.model.get("part1")
+            };
+
+            this.model.set(swapped);
+        },
+
+        remove: function() {
+            this.model.destroy();
         }
+
+
     });
 
     var ListView = Backbone.View.extend({
